c69dfa823b97f1280c28180db3cb74b6fd01d831
Matt Im <matthew.seiim@gmail.com>
2020-05-15 21:37:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,5 +2,5 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-    puts doc.css(".headline-26OIBN")
-doc.css("inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+doc.css(".headline-26OIBN").text
+
